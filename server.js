7793e4b69edd1178263b024addb3f9754cd579b0
Harsh Seth <harshseth2006@gmail.com>
2019-04-21 01:29:04 +0530

    Set up basic Pug server

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
 
-// app.set('views', './views')
-// app.set('view engine', 'pug')
+app.set('views', './views')
+app.set('view engine', 'pug')
 
 const portNum = 3000
+var pageViews = 0
 
 app.get('/', (req, res) => {
-    res.send({'message': 'Server works!'})
+    pageViews += 1
+    res.render('userManagement', {pageViews: pageViews})
 })
 
 app.listen(portNum, () => console.log("Server's up at", portNum))
